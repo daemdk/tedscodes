52b871a3205ebe6b0919b46c0c62680de3657ce2
이대기 <daemdk@msn.com>
2024-04-12 12:17:18 +0900

    Day 1 updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tedscodes
+# Hello tedscodes.
+코드를 아름답다고 생각해 본 적 있나요? 저와 함께 아름다운 코드를 발견하고 즐기는 즐거움을 느꼈으면 좋겠습니다.
+제 웹사이트(Ted's Codes)의 샘플 코드를 정리해 둔 repo 입니다.
+
+# Ted's Codes
+https://www.tedscodes.com/
---
+++ day1-memoization/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Hello Day1");
+
+FibonacciCalculator calculator = new();
+int term = 10;
+long result = calculator.Calculate(term);
+Console.WriteLine($"The results for {term}: {result}");
+
+/// <summary>
+/// Calculate a Fibonacci numbers
+/// </summary>
+class FibonacciCalculator
+{
+
+    // cache for memoization
+    private Dictionary<int, long> _cache = new Dictionary<int, long>();
+
+    /// <summary>
+    /// Calculate the number
+    /// </summary>
+    /// <param name="n">numbers to calculate</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public long Calculate(int n)
+    {
+        if (n < 0) throw new ArgumentException("n must be 0 or greater.");
+        if (n <= 1) return n;
+
+        // Returns if there is a cached value
+        if (_cache.TryGetValue(n, out long value))
+            return value;
+
+        // The Calculate method calls itself to calculate Fibonacci numbers.
+        // This makes the code concise, but can cause performance issues without memoization.
+        value = Calculate(n - 1) + Calculate(n - 2);
+        _cache[n] = value; // Store results in cache
+        return value;
+    }
+}
