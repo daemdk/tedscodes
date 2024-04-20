1ede6ec407ddf761e11eff0a637267a4750964d4
이대기 <daemdk@msn.com>
2024-04-20 11:27:52 +0900

    day7 updated

--- day7-prototype-pattern/DeepPerson.cs
+++ day7-prototype-pattern/DeepPerson.cs
@@ -2,7 +2,6 @@ public class DeepPerson : ICloneable
 {
     public string? Name { get; set; }
     public Address Address { get; set; } = new();
-
     public object Clone()
     {
         var serialized = System.Text.Json.JsonSerializer.Serialize(this);
