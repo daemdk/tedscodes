0d71bb7440fd481285dd119f1d74d59502cd6b9d
이대기 <daemdk@msn.com>
2024-05-14 09:47:10 +0900

    Day13 - fizzbuzz

---
+++ day13-fizzbuzz/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, Day13!");
+
+for (int i = 1; i <= 100; i++)
+{
+    string result = "";
+    if (i % 3 == 0) result += "Fizz";
+    if (i % 5 == 0) result += "Buzz";
+    Console.WriteLine(string.IsNullOrEmpty(result) ? i.ToString() : result);
+}
